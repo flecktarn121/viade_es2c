4af46edf0a3eefeae174821a247c960b0105f22b
Elmer <UO257192@uniovi.es>
2020-02-26 12:55:32 +0100

    NavigationBar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar></NavigationBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -5,28 +5,27 @@ import logo from './icono.png';
 import user from './user.svg';
 import {Nav, Navbar} from 'react-bootstrap';
 
-function Home() {
-    return (
-        <div>
+class NavigationBar extends React.Component {
+    render() {
+        return(
             <Navbar expand="lg" bg="success" variant="dark">
                 <Navbar.Brand href="#home">
                     <img src={logo} width="30" height="30"  className="d-inline-block align-center" alt="logo"/>
                     Viade
                 </Navbar.Brand>
-
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Iniciar ruta</Nav.Link>
-                        <Nav.Link href="#link">Ver rutas</Nav.Link>
+                        <Nav.Link href="#home">Inicio</Nav.Link>
+                        <Nav.Link href="#link">Crear ruta</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 <Navbar.Brand href="#home">
                     <img src={user} width="30" height="30"  className="d-inline-block align-top" alt="logo"/>
                 </Navbar.Brand>
             </Navbar>
-        </div>
-    );
+        );
+    }
 }
 
-export default Home;
+export default NavigationBar;

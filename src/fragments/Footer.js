eebec399a539ae27da3bdb68e519be29905f8a58
Elmer <UO257192@uniovi.es>
2020-02-26 19:08:22 +0100

    Footer fragment

---
+++ src/fragments/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../Home.css';
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <footer className="page-footer font-small Nav pt-4">
+
+                <div className="container">
+                    <p>
+                        Project delevoped by ES2C Arquisoft Team
+                    </p>
+                </div>
+
+                <div className="footer-copyright text-center py-3">© 2020 Copyright
+                </div>
+
+            </footer>
+        );
+    }
+}
+
+export default Footer;
--- src/fragments/NavigationBar.js
+++ src/fragments/NavigationBar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css';
-import logo from './icono.png';
-import user from './user.svg';
+import '../Home.css';
+import logo from '../img/icono.png';
 import {Nav, Navbar} from 'react-bootstrap';
 
 class NavigationBar extends React.Component {
     render() {
         return(
-            <Navbar expand="lg" bg="success" variant="dark">
+            <Navbar fixed="top" expand="lg" bg="success" variant="dark">
                 <Navbar.Brand href="#home">
                     <img src={logo} width="30" height="30"  className="d-inline-block align-center" alt="logo"/>
                     Viade
@@ -18,11 +17,9 @@ class NavigationBar extends React.Component {
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Inicio</Nav.Link>
                         <Nav.Link href="#link">Crear ruta</Nav.Link>
+                        <Nav.Link href="#link">Usuario</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-                <Navbar.Brand href="#home">
-                    <img src={user} width="30" height="30"  className="d-inline-block align-top" alt="logo"/>
-                </Navbar.Brand>
             </Navbar>
         );
     }

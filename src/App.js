23379ad4c5f48a382ea18d3d6e395f38ebf04916
Jaime <jaimeknd77@gmail.com>
2020-02-28 01:00:00 +0100

    Dynamic Map
    Geolocation implemented

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
     <div className="App">
       <NavigationBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React. Developed using Docker.
-        </a>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+              Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+              Learn React. Developed using Docker.
+          </a>
       </header>
     <Footer/>
     </div>

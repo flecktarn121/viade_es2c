cbee1d8cd2170e69f702bfc5b5364360b4cee70b
Jaime <jaimeknd77@gmail.com>
2020-03-02 14:40:31 +0100

    Revert "Merge branch 'frontend' into develop"
    This reverts commit 836b0b2e29f56968c80bde5a38b71b62af9a2547.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import logo from './img/logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import NavigationBar from './fragments/NavigationBar';
-import Footer from "./fragments/Footer";
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar/>
       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-              Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-              Learn React. Developed using Docker.
-          </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React. Developed using Docker.
+        </a>
       </header>
-    <Footer/>
     </div>
   );
 }

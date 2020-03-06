fe255b5ccee56b8f7701c83c374ee8f7ab62eee4
UO257192 <UO257192@uniovi.es>
2020-03-06 14:23:36 +0100

    Integrate Generator React Solid template

--- src/loaders/LoggedIn.jsx
+++
@@ -1,8 +0,0 @@
-import useLoggedIn from './hooks/UseLoggedIn';
-
-/** Pane that only shows its contents when the user is logged in. */
-function LoggedIn({ children = null }) {
-    const loggedIn = useLoggedIn();
-    return loggedIn ? children : null;
-}
-export default  LoggedIn;
--- src/loaders/LoggedOut.jsx
+++
@@ -1,8 +0,0 @@
-import useLoggedOut from './hooks/UseLoggedOut';
-
-/** Pane that only shows its contents when the user is logged out. */
-function LoggedOut({ children = null }) {
-    const loggedOut = useLoggedOut();
-    return loggedOut ? children : null;
-}
-export default LoggedOut;

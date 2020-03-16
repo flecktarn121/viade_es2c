a0cc000083b6fd51d662bbed70a1580c1dd82a55
UO257192 <UO257192@uniovi.es>
2020-03-16 10:13:56 +0100

    Tests for Route Create and TimeLine

---
+++ src/containers/TimeLine/timeline.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {cleanup, render} from 'react-testing-library';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import TimeLine from "./Timeline";
+
+library.add(fas);
+
+describe.only('Timeline', () => {
+  afterAll(cleanup);
+  const { container, getByTestId } = render(
+      <Router>
+        <TimeLine/>
+      </Router>
+  );
+
+  test('renders without crashing', () => {
+    expect(container).toBeTruthy();
+  });
+});
--- src/utils/InOut/FileWriter.js
+++ src/utils/InOut/FileWriter.js
@@ -1,19 +1,18 @@
 import SolidAuth from "solid-auth-client";
-import Route from "../route/Route";
 
 class FileWriter {
-    constructor() {
-    }
 
-  static async handleSave(url,text) {
-            // Not using LDFlex here, because this is not an RDF document.
-            const result = await SolidAuth.fetch(url, {
-                method: 'PUT',
-                body: text,
-                headers: {
-                    'Content-Type': 'text/plain'
-                }
-            });
+    static async handleSave(url, text) {
+        // Not using LDFlex here, because this is not an RDF document.
+        const result = await SolidAuth.fetch(url, {
+            method: 'PUT',
+            body: text,
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        });
+        result();
     }
 }
+
 export default FileWriter;

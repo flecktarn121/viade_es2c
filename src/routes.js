36fc5f2b6f4db67592011f3e243d9a42976e5fb8
UO257192 <UO257192@uniovi.es>
2020-03-18 11:31:17 +0100

    [GH-PAGES] Trying to fix it

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
 import {NotLoggedInLayout, PrivateLayout, PublicLayout} from '@layouts';
-import {BrowserRouter as Router, Redirect, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Switch} from "react-router-dom";
 
 import {
     CreateRoute,
@@ -22,7 +22,8 @@ import FriendsList from './containers/FriendsList/FriendsList';
 
 const privateRoutes = [
     {
-        id: 'welcome',
+        id: 'we' +
+            'lcome',
         path: '/welcome',
         component: Welcome
     },

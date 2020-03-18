634be848f15378171204f9266452594f5d1e7415
AnaGciaSchz <anags.clm@gmail.com>
2020-03-18 19:37:22 +0100

    I put the line that was missing

--- src/utils/parser/RouteToRdfParser.js
+++ src/utils/parser/RouteToRdfParser.js
@@ -6,33 +6,33 @@ class RouteToRdfParser {
         this.webID = webID;
     }
     parse(){
-        var prefixs = this.getPrefix();
-        var information = this.getInformation();
-        var viadePoints = this.getViadePoints();
-        var media = this.getMedia();
-        alert("asdasds")
+        let prefixs = this.getPrefix();
+        let information = this.getInformation();
+        let viadePoints = this.getViadePoints();
+        let media = this.getMedia();
         FileWriter.handleSave(this.webID+"viade/"+this.route.name,(String)(prefixs+information+viadePoints+media))
     }
 
     getPrefix(){
-        var viade = "prefix viade:  <http://arquisoft.github.io/viadeSpec/>\n";
-        var schema = "prefix schema: <http://schema.org/>\n";
-        var rdfs = "prefix rdfs:   <http://www.w3.org/2000/01/rdf-schema#>\n";
-        var xsd = "prefix xsd:    <http://www.w3.org/2001/XMLSchema#>\n";
-        return (String)(viade+schema+rdfs+xsd);
+        let line = "prefix :   <http://example.org/> \n";
+        let viade = "prefix viade:  <http://arquisoft.github.io/viadeSpec/>\n";
+        let schema = "prefix schema: <http://schema.org/>\n";
+        let rdfs = "prefix rdfs:   <http://www.w3.org/2000/01/rdf-schema#>\n";
+        let xsd = "prefix xsd:    <http://www.w3.org/2001/XMLSchema#>\n";
+        return (String)(line+viade+schema+rdfs+xsd);
     }
     getInformation(){
-        var myRoute = ":myRoute a viade:Route ;\n";
-        var name ="schema:name \""+this.route.name+"\" ;\n";
-        var description = "schema:description \""+this.route.description+"\" ;\n";
+        let myRoute = ":myRoute a viade:Route ;\n";
+        let name ="schema:name \""+this.route.name+"\" ;\n";
+        let description = "schema:description \""+this.route.description+"\" ;\n";
         return (String)(myRoute+name+description);
     }
 
     getViadePoints(){
-        var cabecera = "viade:points (\n";
-        var final = " );\n";
-        var puntos = "";
-        var i;
+        let cabecera = "viade:points (\n";
+        let final = " );\n";
+        let puntos = "";
+        let i;
         for (i = 0; i < this.route.points.length; i++) {
             puntos += "[ schema:latitude "+this.route.points[i].position.lat+" ; schema:longitude "+this.route.points[i].position.lng+" ]\n";
         }
@@ -41,7 +41,7 @@ class RouteToRdfParser {
     }
 
     getMedia(){
-        var comments = "viade:hasComments \""+this.route.comments+"\" ;\n";
+        let comments = "viade:hasComments \""+this.route.comments+"\" ;\n";
         return comments;
     }
 

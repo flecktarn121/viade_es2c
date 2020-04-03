c3a9a32862e1cbd8b40bbcd449064f4af33840b1
UO257192 <UO257192@uniovi.es>
2020-04-03 12:35:46 +0200

    Trying to fix View Route test

--- src/containers/Ruta/Ruta.test.js
+++ src/containers/Ruta/Ruta.test.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {cleanup, render} from 'react-testing-library';
 import Ruta from "./Ruta";
 
+const match = {
+  params: {id: '0'}
+};
+
 describe.only('Ruta', () => {
   afterAll(cleanup);
 
   const { container, getByTestId } = render(
-    <Ruta />
+    <Ruta {...{match }}/>
   );
 
   test('renders without crashing', () => {

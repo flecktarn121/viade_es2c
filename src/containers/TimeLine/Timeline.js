d87368eee874158658412e135a3a89cb3419522d
AnaGciaSchz <anags.clm@gmail.com>
2020-04-04 21:03:04 +0200

    Test with internationalization in TimeLine

--- src/containers/TimeLine/Timeline.js
+++ src/containers/TimeLine/Timeline.js
@@ -4,13 +4,18 @@ import {Header, TimelineContainer, TimelineWrapper} from './timeline.style';
 import RdftoRouteParser from "../../utils/parser/RdfToRouteParser";
 import SmallRow from "../Delay/Delay";
 import {Loader} from '@util-components';
+//import {useTranslation} from 'react-i18next';
+import {withTranslation} from 'react-i18next';
+import {RegisterComponent} from "../Register/register.component";
 
-type Props = { webId: String };
+type Props = {
+    webId: String,
+    t: Function
+};
 
 /**
  * Container component for the Welcome Page, containing example of how to fetch data from a POD
  */
-
 class TimeLine extends React.Component {
 
     state = {isLoading: true};
@@ -25,12 +30,13 @@ class TimeLine extends React.Component {
     loaded = () => this.setState({isLoading: false});
 
     render() {
+        const { providers, t } = this.props;
         setTimeout(this.loaded, 3000);
         return (
             <TimelineWrapper data-testid="timeline-wrapper">
                 <TimelineContainer data-testid="timeline-container">
                     <Header data-testid="timeline-header">
-                        <h1>Ver Rutas</h1>
+                        <h1>{t('timeline.seeRoutes')}</h1>
                     </Header>
                     <SmallRow webID={this.webID}/>
                 </TimelineContainer>
@@ -41,4 +47,5 @@ class TimeLine extends React.Component {
     }
 }
 
-export default TimeLine;
+export {TimeLine};
+export default withTranslation()(TimeLine);

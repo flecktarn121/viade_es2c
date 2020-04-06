cd2fd16e88eb0bb621852577a73b1fab4fdbfbec
UO257192 <UO257192@uniovi.es>
2020-04-06 14:19:15 +0200

    Geojson test

--- src/containers/CreateRouteGeoJSON/CreateRouteGeoJSON.js
+++ src/containers/CreateRouteGeoJSON/CreateRouteGeoJSON.js
@@ -30,7 +30,7 @@ const CreateRouteGeoJSON = ({ webId }: Props) => {
             if(features[0].geometry.type === "LineString"){
                 var coordinates = features[0].geometry.coordinates;
                 for(var i = 0; i< coordinates.length; i++){
-                    markers.push({lat:coordinates[i][0], lng: coordinates[i][1]});
+                    markers.push({position:{lat:coordinates[i][0], lng: coordinates[i][1]}});
                 }
             }
         }

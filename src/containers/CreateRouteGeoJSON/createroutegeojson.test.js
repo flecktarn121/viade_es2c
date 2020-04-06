39d47f8f77a374d311a48d78c27e368aa48f5717
UO257192 <UO257192@uniovi.es>
2020-04-06 14:39:20 +0200

    Geojson test

--- src/containers/CreateRouteGeoJSON/createroutegeojson.test.js
+++ src/containers/CreateRouteGeoJSON/createroutegeojson.test.js
@@ -45,13 +45,12 @@ describe.only('CreateRouteGeoJSON', () => {
         const input_description = getByTestId(container, 'input-description');
         const input_file = getByTestId(container, 'input-file');
         const button_save = getByTestId(container, 'button-save');
-        const file = new File(["{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[28.67431640625,51.74743863117572],[28.037109375,50.33844888725473],[30.684814453125004,50.00067775723633],[30.223388671874996,51.303145259199056],[29.68505859375,49.1888842152458],[26.400146484375,51.31688050404585]]}}]}"], "track.geojson");
+        const file = new File(['{"type": "FeatureCollection","features": [{"type": "Feature","properties": {},"geometry": {"type": "LineString","coordinates": [[28.67431640625,51.74743863117572],[28.037109375,50.33844888725473]]}}]}'], "track.geojson");
         fireEvent.change(input_title, { target: { value: "prueba" } });
         fireEvent.change(input_description, { target: { value: "prueba" } });
         Object.defineProperty(input_file, "files", {
             value: [file]
         });
-
         fireEvent.change(input_file);
         expect(input_title.value).toEqual("prueba");
         expect(input_description.value).toEqual("prueba");

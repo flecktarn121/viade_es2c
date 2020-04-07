6e2c6866a538adddcd74b0ed932f91017628bd51
UO257192 <UO257192@uniovi.es>
2020-04-07 13:04:42 +0200

    Test for media loaders

--- src/containers/CreateRouteGPX/createroutegpx.test.js
+++ src/containers/CreateRouteGPX/createroutegpx.test.js
@@ -30,6 +30,8 @@ describe.only('CreateRouteGPX', () => {
         const input_description = getByTestId(container, 'input-description');
         const input_file = getByTestId(container, 'input-file');
         const button_save = getByTestId(container, 'button-save');
+        const input_img = getByTestId(container, 'input-img');
+        const input_video = getByTestId(container, 'input-video');
 
         expect(route_wrapper).not.toBe(null);
         expect(route_header).not.toBe(null);
@@ -37,6 +39,8 @@ describe.only('CreateRouteGPX', () => {
         expect(input_description).not.toBe(null);
         expect(input_file).not.toBe(null);
         expect(button_save).not.toBe(null);
+        expect(input_img).not.toBe(null);
+        expect(input_video).not.toBe(null);
 
     });
 
@@ -69,6 +73,24 @@ describe.only('CreateRouteGPX', () => {
         fireEvent.change(input_file);
         expect(input_title.value).toEqual("prueba");
         expect(input_description.value).toEqual("prueba");
+
+        const input_img = getByTestId(container, 'input-img');
+        const input_video = getByTestId(container, 'input-video');
+
+        const img = new File(["(⌐□_□)"], "img.png", {
+            type: "image/png"
+        });
+
+        const video = new File(["(⌐□_□)"], "video.mp4", {
+            type: "video/mp4"
+        });
+        Object.defineProperty(input_img, "files", {
+            value: [img]
+        });
+
+        Object.defineProperty(input_video, "files", {
+            value: [video]
+        });
 
         fireEvent.click(button_save);
 

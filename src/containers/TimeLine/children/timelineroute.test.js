2106ff51ba66f52ecf36ad7cac4badac70384c19
UO257192 <UO257192@uniovi.es>
2020-04-09 16:25:43 +0200

    Coverage

--- src/containers/TimeLine/children/timelineroute.test.js
+++ src/containers/TimeLine/children/timelineroute.test.js
@@ -9,10 +9,10 @@ import Route from "../../../utils/route/Route";
 library.add(fas);
 
 const markers = [
-  {lat: 43.354831, lng: -5.851303},
-  {lat: 43.356440, lng: -5.854693},
-  {lat: 43.361836, lng: -5.850547}
-]
+  {position :{lat: 43.354831, lng: -5.851303}},
+  {position :{lat: 43.356440, lng: -5.854693}},
+  {position :{lat: 43.361836, lng: -5.850547}}
+];
 
 const props = {
   title: "Prueba",
@@ -20,9 +20,9 @@ const props = {
   author: "Prueba",
   description: "Prueba",
   markers : [
-    {lat: 43.354831, lng: -5.851303},
-    {lat: 43.356440, lng: -5.854693},
-    {lat: 43.361836, lng: -5.850547}
+    {position :{lat: 43.354831, lng: -5.851303}},
+    {position :{lat: 43.356440, lng: -5.854693}},
+    {position :{lat: 43.361836, lng: -5.850547}}
   ],
   ruta: new Route("Prueba", "Prueba", markers, "Prueba", null, null, null, null)
 };

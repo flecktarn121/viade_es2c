7627889d123e3efb568ba8172d5dd8c6ac4c1467
UO257192 <UO257192@uniovi.es>
2020-04-09 16:10:23 +0200

    Coverage

--- src/containers/Ruta/Ruta.test.js
+++ src/containers/Ruta/Ruta.test.js
@@ -3,23 +3,23 @@ import {cleanup, render} from 'react-testing-library';
 import Ruta from "./Ruta";
 import Route from "../../utils/route/Route"
 
-const match = {
-  params: {id: '0'}
-};
-
 const markers = [
     {position :{lat: 43.354831, lng: -5.851303}},
     {position :{lat: 43.356440, lng: -5.854693}},
     {position :{lat: 43.361836, lng: -5.850547}}
 ];
 
-const ruta = new Route("prueba", "prueba", markers, null, null, null);
+const ruta = new Route("prueba", "prueba", markers, null, null, null, null, "");
+
+const props = {
+    route: ruta
+};
 
 describe.only('Ruta', () => {
   afterAll(cleanup);
 
   const { container, getByTestId } = render(
-    <Ruta {...{match,ruta }}/>
+    <Ruta {...{...props }}/>
   );
 
   test('renders without crashing', () => {
